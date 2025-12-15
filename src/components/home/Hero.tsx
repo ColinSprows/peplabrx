1a047db625237b3c8859230b5d11b11e85222e26
ColinSprows <colin.sprows@gmail.com>
2025-12-14 20:09:27 -0500

    Make vials 2x bigger and vertically center them

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -28,14 +28,14 @@ export default function Hero() {
           </motion.div>
 
           {/* Hero image - 2 vials arrangement */}
-          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 0.2 }} className="relative flex items-end justify-center h-[450px] md:h-[500px]">
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 0.2 }} className="relative flex items-center justify-center h-[450px] md:h-[500px]">
             {/* Vial 1 - back left */}
-            <div className="absolute left-1/2 bottom-0 -translate-x-[60%] z-10">
-              <Image src="/vial.png" alt="Research Peptide Vial" width={320} height={320} className="w-52 md:w-64 h-auto object-contain opacity-90" />
+            <div className="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-[75%] z-10">
+              <Image src="/vial.png" alt="Research Peptide Vial" width={640} height={640} className="w-[26rem] md:w-[32rem] h-auto object-contain opacity-90" />
             </div>
             {/* Vial 2 - front center */}
-            <div className="absolute left-1/2 bottom-0 -translate-x-[35%] z-20">
-              <Image src="/vial2.png" alt="Premium Research Peptides" width={380} height={380} className="w-64 md:w-80 h-auto object-contain" priority />
+            <div className="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-[35%] z-20">
+              <Image src="/vial2.png" alt="Premium Research Peptides" width={760} height={760} className="w-[32rem] md:w-[40rem] h-auto object-contain" priority />
             </div>
           </motion.div>
         </div>
